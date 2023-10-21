e3c3e83efb30a5b388af89cc09449bb3079e4790
Alitor004 <irmaosgomes1302@gmail.com>
2023-10-20 23:03:39 -0300

    Exeção de exemplo com dados aleatorio

--- WebAPI.Tests/WeatherForecastTests.cs
+++ WebAPI.Tests/WeatherForecastTests.cs
@@ -1,3 +1,4 @@
+using System;
 using WebAPI.Controllers;
 using WebAPI.Exceptions;
 
@@ -12,14 +13,55 @@ public class WeatherForecastTests
     {
         _weatherForecastService = new WeatherForecastService();
     }
+
     [TestMethod]
     public void ShouldThrowAnExceptionWhenCityIsDifferentOfSP()
     {
         const string cityDiffSP = "Gru";
 
-        Assert.ThrowsException<CityNotFoundException>(() => 
+        Assert.ThrowsException<CityNotFoundException>(() =>
             _weatherForecastService.GetTemperature(cityDiffSP)
         );
-        
+
     }
+
+    [TestMethod]
+    public void ShouldReturnAnArrayOfTempeture()
+    {
+        // AAA
+        string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        List<WeatherForecast> temperatures = new List<WeatherForecast>
+        {
+            new WeatherForecast {
+            Date = DateTime.Now.AddDays(1),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            },
+
+            new WeatherForecast {
+            Date = DateTime.Now.AddDays(1),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            },
+
+            new WeatherForecast {
+            Date = DateTime.Now.AddDays(1),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            },
+        };
+
+        var a = _weatherForecastService.GetTemperature("SP");
+        Assert.AreEqual(temperatures.Count, 3);
+
+        Assert.AreEqual(a, temperatures);
+    }
+
 }
+
+
+
